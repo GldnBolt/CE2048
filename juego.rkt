a7f1847d7e1a6f00ae9709ea4a40107876d43792
FabiMC040105 <149991170+FabiMC040105@users.noreply.github.com>
2026-04-09 01:35:08 -0600

    suma

--- juego.rkt
+++ juego.rkt
@@ -2,58 +2,102 @@
 (require 2htdp/universe) ;efectos de movimiento
 (require 2htdp/image) ;cargar imagenes
 (require (lib "graphics.ss" "graphics")) ;libreria para graficos simples
-(open-graphics) ;abir libreria
+(require "logica.rkt") ;archivo de logica
+(open-graphics) ;abrir libreria
 
 
 ;DEFINICION DE VENTANAS
-(define ventana (open-viewport "JUEGO 2048" 800 500)) ;para ventana tipo viewport
-(define oculta (open-pixmap "JUEGO 2048" 800 500)) ; lo mismo pero esta no se ve, es donde estan las operaciones y luego se reflejan en la viewport
+(define ventana (open-viewport "JUEGO 2048" 800 500)) ;ventana visible
+(define oculta (open-pixmap "JUEGO 2048" 800 500)) ;ventana oculta para dibujar y luego copiar
 
-;DIBUJO DEL FONDO (se hace primero para que este atras)
-((draw-solid-rectangle oculta) (make-posn 0 0) 800 500 "ivory")
-((draw-solid-rectangle oculta) (make-posn 200 50) 400 400 "tan")
 
 ;VENTANA DE BIENVENIDA
-;necesito poner este "contenedor" para que ejecute varias instrucciones una tras otra para que parezcan una sola, lo hago para que pueda meterle varios parametros a la letra por ejemplo
-(define ventana2 (open-viewport "BIENVENIDOS" 400 250))
-   (begin
-     ((draw-viewport ventana2) "hotpink")
-     ((draw-string ventana2) (make-posn 94 120) "BIENVENIDOS AL JUEGO 2048" "white")
-   (sleep 3)
-   (close-viewport ventana2)
-    )
+(define (mostrar-bienvenida)
+  (define ventana2 (open-viewport "BIENVENIDOS" 400 250))
+  (begin
+    ((draw-viewport ventana2) "hotpink")
+    ((draw-string ventana2) (make-posn 94 120) "BIENVENIDOS AL JUEGO 2048" "white")
+    (sleep 3)
+    (close-viewport ventana2)))
 
-; FUNCION PARA EL TABLERO (las lineas)
-; n es el ancho que me dice cuantas columnas van a haber y m cuantas filas van a haber
+
+;DIBUJO DEL FONDO
+(define (dibujar-fondo)
+  ((draw-solid-rectangle oculta) (make-posn 0 0) 800 500 "ivory")
+  ((draw-solid-rectangle oculta) (make-posn 200 50) 400 400 "tan"))
+
+
+;FUNCION PARA EL TABLERO (las lineas)
 (define (dibujar-tablero m n)
   (let ([ancho (/ 400 n)]
         [alto (/ 400 m)])
     (for ([i (in-range n)])
       (for ([j (in-range m)])
-        ((draw-rectangle oculta) (make-posn (+ 200 (* i ancho)) (+ 50 (* j alto))) ancho alto "black")))))
-
-;EJECUTANDO DIBUJO Y AZAR
-;defino aca m y n para que siempre use el mismo y aparte llamo a la funcion que dibuja
-(define m 6)
-(define n 5)
-(dibujar-tablero m n)
-
-
-;LOGICA DE LOS MUMEROS 2
-(define ancho-celda (/ 400 n)) ;tengo que hacerlas de nuevo porque las otras eran privadas para la funcion de dibujar tablero
-(define alto-celda (/ 400 m))
-
-;PRIMER 2
-(define col1 (random n))
-(define fil1 (random m))
-((draw-string oculta) (make-posn(+ 200 (* col1 ancho-celda) (/ ancho-celda 2.5)) (+ 50 (* fil1 alto-celda) (/ alto-celda 1.5)))"2" "black")
+        ((draw-rectangle oculta)
+         (make-posn (+ 200 (* i ancho))
+                    (+ 50 (* j alto)))
+         ancho
+         alto
+         "black")))))
 
 
-;SEGUNDO 2
-(define col2 (random n))
-(define fil2 (random m))
-((draw-string oculta) (make-posn(+ 200 (* col2 ancho-celda) (/ ancho-celda 2.5)) (+ 50 (* fil2 alto-celda) (/ alto-celda 1.5)))"2" "black")
+;AJUSTE PARA CENTRAR MEJOR LOS NUMEROS
+(define (ajuste-x valor)
+  (cond
+    [(< valor 10) (/ ancho-celda 2.5)]
+    [(< valor 100) (/ ancho-celda 3.3)]
+    [(< valor 1000) (/ ancho-celda 4.2)]
+    [else (/ ancho-celda 5.2)]))
+
+;DIBUJAR UN NUMERO EN UNA CELDA
+(define (dibujar-numero col fil valor)
+  ((draw-string oculta)
+   (make-posn (+ 200 (* col ancho-celda) (ajuste-x valor))
+              (+ 50 (* fil alto-celda) (/ alto-celda 1.5)))
+   (number->string valor)
+   "black"))
+
+;DIBUJAR TODOS LOS NUMEROS DEL TABLERO
+(define (dibujar-numeros tablero)
+  (for ([fil (in-range m)])
+    (for ([col (in-range n)])
+      (define valor (valor-en tablero fil col))
+      (when (not (= valor 0))
+        (dibujar-numero col fil valor)))))
 
 
+;REDIBUJAR TODO
+(define (redibujar tablero)
+  (dibujar-fondo)
+  (dibujar-tablero m n)
+  (dibujar-numeros tablero)
+  (copy-viewport oculta ventana))
 
-(copy-viewport oculta ventana)
+
+;REVISAR SI LA TECLA ES UNA FLECHA
+(define (tecla-flecha? tecla)
+  (or (eq? tecla 'left)
+      (eq? tecla 'right)
+      (eq? tecla 'up)
+      (eq? tecla 'down)))
+
+
+;BUCLE PRINCIPAL DEL JUEGO
+(define (bucle-juego tablero)
+  (redibujar tablero)
+  (define tecla (key-value (get-key-press ventana)))
+  (cond
+    [(tecla-flecha? tecla)
+     (bucle-juego (mover-y-crear tablero tecla))]
+    [(and (char? tecla)
+          (or (char=? tecla #\q)
+              (char=? tecla #\Q)))
+     (close-graphics)]
+    [else
+     (bucle-juego tablero)]))
+
+
+;INICIO
+(mostrar-bienvenida)
+(define tablero (tablero-inicial))
+(bucle-juego tablero)
--- logica.rkt
+++ logica.rkt
@@ -1,20 +1,134 @@
 #lang racket
 
-(provide m n ancho-celda alto-celda col1 fil1 col2 fil2)
+(provide m
+         n
+         ancho-celda
+         alto-celda
+         tablero-inicial
+         mover-tablero
+         mover-y-crear
+         valor-en)
 
-;EJECUTANDO DIBUJO Y AZAR
-;defino aca m y n para que siempre use el mismo
-(define m 6)
-(define n 5)
+;TAMAÑO DEL TABLERO
+(define m 4) ; filas
+(define n 4) ; columnas
 
-;LOGICA DE LOS NUMEROS 2
+;MEDIDAS DE CADA CELDA
 (define ancho-celda (/ 400 n))
 (define alto-celda (/ 400 m))
 
-;PRIMER 2
-(define col1 (random n))
-(define fil1 (random m))
+;TABLERO VACIO
+(define (fila-vacia)
+  (make-list n 0))
 
-;SEGUNDO 2
-(define col2 (random n))
-(define fil2 (random m))
+(define (tablero-vacio)
+  (build-list m (lambda (x) (fila-vacia))))
+
+;REEMPLAZAR UN VALOR EN UNA LISTA
+(define (reemplazar-en-lista lst pos valor)
+  (cond
+    [(null? lst) '()]
+    [(zero? pos) (cons valor (cdr lst))]
+    [else (cons (car lst)
+                (reemplazar-en-lista (cdr lst) (sub1 pos) valor))]))
+
+;PONER UN VALOR EN EL TABLERO
+(define (poner-valor tablero fil col valor)
+  (reemplazar-en-lista
+   tablero
+   fil
+   (reemplazar-en-lista (list-ref tablero fil) col valor)))
+
+;OBTENER VALOR DE UNA POSICION
+(define (valor-en tablero fil col)
+  (list-ref (list-ref tablero fil) col))
+
+;BUSCAR POSICIONES VACIAS
+(define (posiciones-vacias tablero)
+  (for*/list ([fil (in-range m)]
+              [col (in-range n)]
+              #:when (= (valor-en tablero fil col) 0))
+    (list fil col)))
+
+;AGREGAR UN 2 EN UNA POSICION VACIA ALEATORIA
+(define (agregar-2-aleatorio tablero)
+  (define vacias (posiciones-vacias tablero))
+  (if (null? vacias)
+      tablero
+      (let* ([p (list-ref vacias (random (length vacias)))]
+             [fil (first p)]
+             [col (second p)])
+        (poner-valor tablero fil col 2))))
+
+;TABLERO INICIAL CON DOS 2
+(define (tablero-inicial)
+  (agregar-2-aleatorio
+   (agregar-2-aleatorio
+    (tablero-vacio))))
+
+;QUITAR CEROS DE UNA FILA
+(define (quitar-ceros fila)
+  (filter (lambda (x) (not (= x 0))) fila))
+
+;FUSIONAR NUMEROS IGUALES UNA SOLA VEZ
+;Ejemplo: (2 2 2 0 0) -> (4 2)
+(define (fusionar fila)
+  (cond
+    [(null? fila) '()]
+    [(null? (cdr fila)) fila]
+    [(= (car fila) (cadr fila))
+     (cons (+ (car fila) (cadr fila))
+           (fusionar (cddr fila)))]
+    [else
+     (cons (car fila)
+           (fusionar (cdr fila)))]))
+
+;RELLENAR CON CEROS AL FINAL
+(define (rellenar-con-ceros fila largo)
+  (append fila (make-list (- largo (length fila)) 0)))
+
+;MOVER UNA FILA A LA IZQUIERDA
+(define (mover-fila-izquierda fila)
+  (rellenar-con-ceros
+   (fusionar (quitar-ceros fila))
+   (length fila)))
+
+;MOVER UNA FILA A LA DERECHA
+(define (mover-fila-derecha fila)
+  (reverse (mover-fila-izquierda (reverse fila))))
+
+;TRANSPONER MATRIZ
+(define (transponer matriz)
+  (apply map list matriz))
+
+;MOVER TABLERO SEGUN DIRECCION
+(define (mover-izquierda tablero)
+  (map mover-fila-izquierda tablero))
+
+(define (mover-derecha tablero)
+  (map mover-fila-derecha tablero))
+
+(define (mover-arriba tablero)
+  (transponer
+   (map mover-fila-izquierda
+        (transponer tablero))))
+
+(define (mover-abajo tablero)
+  (transponer
+   (map mover-fila-derecha
+        (transponer tablero))))
+
+(define (mover-tablero tablero direccion)
+  (cond
+    [(eq? direccion 'left)  (mover-izquierda tablero)]
+    [(eq? direccion 'right) (mover-derecha tablero)]
+    [(eq? direccion 'up)    (mover-arriba tablero)]
+    [(eq? direccion 'down)  (mover-abajo tablero)]
+    [else tablero]))
+
+;MOVER Y CREAR UN NUEVO 2 SOLO SI EL TABLERO CAMBIO
+(define (mover-y-crear tablero direccion)
+  (define nuevo-tablero (mover-tablero tablero direccion))
+  (if (equal? nuevo-tablero tablero)
+      tablero
+      (agregar-2-aleatorio nuevo-tablero)))
