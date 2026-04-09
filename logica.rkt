2e6e8513b84a39f59f0431c8deb58ee9e095c33c
FabiMC040105 <149991170+FabiMC040105@users.noreply.github.com>
2026-04-09 01:08:58 -0600

    juntado eh

--- logica.rkt
+++ logica.rkt
@@ -1,1 +1,20 @@
 #lang racket
+
+(provide m n ancho-celda alto-celda col1 fil1 col2 fil2)
+
+;EJECUTANDO DIBUJO Y AZAR
+;defino aca m y n para que siempre use el mismo
+(define m 6)
+(define n 5)
+
+;LOGICA DE LOS NUMEROS 2
+(define ancho-celda (/ 400 n))
+(define alto-celda (/ 400 m))
+
+;PRIMER 2
+(define col1 (random n))
+(define fil1 (random m))
+
+;SEGUNDO 2
+(define col2 (random n))
+(define fil2 (random m))
