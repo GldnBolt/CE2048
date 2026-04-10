d5ec0e3ed68c281f64cbea6519614edc537a0f4c
FabiMC040105 <149991170+FabiMC040105@users.noreply.github.com>
2026-04-09 19:18:17 -0600

    score incorporado

--- logica.rkt
+++ logica.rkt
@@ -11,6 +11,8 @@
          tablero-inicial
          mover-tablero
          mover-y-crear
+         resultado-tablero
+         resultado-puntos
          valor-en
          gano?
          perdio?)
@@ -28,6 +30,13 @@
 (define ancho-celda (/ ancho-tablero n))
 (define alto-celda (/ alto-tablero m))
 
+; ACCESORES DE RESULTADOS
+(define (resultado-tablero resultado)
+  (car resultado))
+
+(define (resultado-puntos resultado)
+  (cadr resultado))
+
 ; CREACION DE LISTAS Y TABLERO
 (define (crear-ceros cantidad)
   (cond
@@ -43,16 +52,18 @@
 (define (tablero-vacio-aux filas)
   (cond
     [(zero? filas) '()]
-    [else (cons (fila-vacia)
-                (tablero-vacio-aux (sub1 filas)))]))
+    [else
+     (cons (fila-vacia)
+           (tablero-vacio-aux (sub1 filas)))]))
 
 ; ACCESO Y MODIFICACION
 (define (reemplazar-en-lista lst pos valor)
   (cond
     [(null? lst) '()]
     [(zero? pos) (cons valor (cdr lst))]
-    [else (cons (car lst)
-                (reemplazar-en-lista (cdr lst) (sub1 pos) valor))]))
+    [else
+     (cons (car lst)
+           (reemplazar-en-lista (cdr lst) (sub1 pos) valor))]))
 
 (define (poner-valor tablero fil col valor)
   (reemplazar-en-lista
@@ -135,46 +146,91 @@
      (cons (car fila)
            (quitar-ceros (cdr fila)))]))
 
-(define (fusionar fila)
-  (cond
-    [(null? fila) '()]
-    [(null? (cdr fila)) fila]
-    [(= (car fila) (cadr fila))
-     (cons (+ (car fila) (cadr fila))
-           (fusionar (cddr fila)))]
-    [else
-     (cons (car fila)
-           (fusionar (cdr fila)))]))
-
 (define (rellenar-con-ceros fila largo)
   (append fila
           (crear-ceros (- largo (length fila)))))
 
+; FUSION Y PUNTAJE
+(define (fusionar-y-puntuar fila)
+  (cond
+    [(null? fila) (list '() 0)]
+    [(null? (cdr fila)) (list fila 0)]
+    [(= (car fila) (cadr fila))
+     (fusionar-y-puntuar-union
+      (+ (car fila) (cadr fila))
+      (fusionar-y-puntuar (cddr fila)))]
+    [else
+     (fusionar-y-puntuar-normal
+      (car fila)
+      (fusionar-y-puntuar (cdr fila)))]))
+
+(define (fusionar-y-puntuar-union suma resultado-resto)
+  (list
+   (cons suma (resultado-tablero resultado-resto))
+   (+ suma (resultado-puntos resultado-resto))))
+
+(define (fusionar-y-puntuar-normal primero resultado-resto)
+  (list
+   (cons primero (resultado-tablero resultado-resto))
+   (resultado-puntos resultado-resto)))
+
 ; MOVIMIENTO DE FILAS
 (define (mover-fila-izquierda fila)
-  (rellenar-con-ceros
-   (fusionar (quitar-ceros fila))
-   (length fila)))
+  (resultado-tablero (mover-fila-izquierda-resultado fila)))
+
+(define (mover-fila-izquierda-resultado fila)
+  (mover-fila-izquierda-resultado-aux
+   fila
+   (fusionar-y-puntuar (quitar-ceros fila))))
+
+(define (mover-fila-izquierda-resultado-aux fila resultado)
+  (list
+   (rellenar-con-ceros
+    (resultado-tablero resultado)
+    (length fila))
+   (resultado-puntos resultado)))
 
 (define (mover-fila-derecha fila)
-  (invertir
-   (mover-fila-izquierda
-    (invertir fila))))
+  (resultado-tablero (mover-fila-derecha-resultado fila)))
+
+(define (mover-fila-derecha-resultado fila)
+  (mover-fila-derecha-resultado-aux
+   (mover-fila-izquierda-resultado (invertir fila))))
+
+(define (mover-fila-derecha-resultado-aux resultado)
+  (list
+   (invertir (resultado-tablero resultado))
+   (resultado-puntos resultado)))
 
 ; MOVIMIENTO DE TABLEROS
 (define (mover-izquierda tablero)
+  (resultado-tablero (mover-izquierda-resultado tablero)))
+
+(define (mover-izquierda-resultado tablero)
   (cond
-    [(null? tablero) '()]
+    [(null? tablero) (list '() 0)]
     [else
-     (cons (mover-fila-izquierda (car tablero))
-           (mover-izquierda (cdr tablero)))]))
+     (combinar-resultados-tablero
+      (mover-fila-izquierda-resultado (car tablero))
+      (mover-izquierda-resultado (cdr tablero)))]))
 
 (define (mover-derecha tablero)
+  (resultado-tablero (mover-derecha-resultado tablero)))
+
+(define (mover-derecha-resultado tablero)
   (cond
-    [(null? tablero) '()]
+    [(null? tablero) (list '() 0)]
     [else
-     (cons (mover-fila-derecha (car tablero))
-           (mover-derecha (cdr tablero)))]))
+     (combinar-resultados-tablero
+      (mover-fila-derecha-resultado (car tablero))
+      (mover-derecha-resultado (cdr tablero)))]))
+
+(define (combinar-resultados-tablero resultado-fila resultado-resto)
+  (list
+   (cons (resultado-tablero resultado-fila)
+         (resultado-tablero resultado-resto))
+   (+ (resultado-puntos resultado-fila)
+      (resultado-puntos resultado-resto))))
 
 (define (primeros matriz)
   (cond
@@ -198,31 +254,50 @@
      (cons (primeros matriz)
            (transponer (restos matriz)))]))
 
+(define (transponer-resultado resultado)
+  (list
+   (transponer (resultado-tablero resultado))
+   (resultado-puntos resultado)))
+
 (define (mover-arriba tablero)
-  (transponer
-   (mover-izquierda
+  (resultado-tablero (mover-arriba-resultado tablero)))
+
+(define (mover-arriba-resultado tablero)
+  (transponer-resultado
+   (mover-izquierda-resultado
     (transponer tablero))))
 
 (define (mover-abajo tablero)
-  (transponer
-   (mover-derecha
+  (resultado-tablero (mover-abajo-resultado tablero)))
+
+(define (mover-abajo-resultado tablero)
+  (transponer-resultado
+   (mover-derecha-resultado
     (transponer tablero))))
 
-(define (mover-tablero tablero direccion)
+(define (mover-tablero-resultado tablero direccion)
   (cond
-    [(eq? direccion 'left) (mover-izquierda tablero)]
-    [(eq? direccion 'right) (mover-derecha tablero)]
-    [(eq? direccion 'up) (mover-arriba tablero)]
-    [(eq? direccion 'down) (mover-abajo tablero)]
-    [else tablero]))
+    [(eq? direccion 'left) (mover-izquierda-resultado tablero)]
+    [(eq? direccion 'right) (mover-derecha-resultado tablero)]
+    [(eq? direccion 'up) (mover-arriba-resultado tablero)]
+    [(eq? direccion 'down) (mover-abajo-resultado tablero)]
+    [else (list tablero 0)]))
+
+(define (mover-tablero tablero direccion)
+  (resultado-tablero
+   (mover-tablero-resultado tablero direccion)))
 
 (define (mover-y-crear tablero direccion)
-  (mover-y-crear-aux tablero (mover-tablero tablero direccion)))
+  (mover-y-crear-aux tablero (mover-tablero-resultado tablero direccion)))
 
-(define (mover-y-crear-aux tablero nuevo-tablero)
+(define (mover-y-crear-aux tablero resultado)
   (cond
-    [(equal? nuevo-tablero tablero) tablero]
-    [else (agregar-baldosa-aleatoria nuevo-tablero)]))
+    [(equal? (resultado-tablero resultado) tablero)
+     (list tablero 0)]
+    [else
+     (list
+      (agregar-baldosa-aleatoria (resultado-tablero resultado))
+      (resultado-puntos resultado))]))
 
 ; CONDICIONES DEL JUEGO
 (define (fila-contiene? fila valor)
